338fe41604111a7ab9d31e30cc6d03eb065a3343
mizukinakano <nakano.mizuki.57r@st.kyoto-u.ac.jp>
2018-10-12 18:00:04 +0900

    中野水貴

--- fibonacci.py
+++ fibonacci.py
@@ -9,7 +9,7 @@ def fib(n):
     a,b=0,1
     for i in range (n):
         a,b=b,a+b
-    return b
+    return a
 print([fib(i) for i in range(10)])
 
 print("フィボナッチ数列の2018番目は" + str(fib(2018)) + "です。")
